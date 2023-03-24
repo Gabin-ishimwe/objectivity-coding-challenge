e324d9b85dc079eb43944ad77f7a420c0d038f37
gabin-ishimwe <g.ishimwe@alustudent.com>
2023-03-24 14:24:10 +0200

    ch: comment code and refactor

--- array_element_swapper.js
+++ array_element_swapper.js
@@ -1,10 +1,22 @@
 function swapArr(arr, m, n) {
+
+  // check if indexes provided don't exceed the arr length
   if (m > arr.length || n > arr.length) return "Index out of range";
+
+  // store value at index m in a temporary variable
   let temp = arr[m];
+
+  // store value at index n in place of value at index m
   arr[m] = arr[n];
+
+  // store the temporary variable in value of index n
   arr[n] = temp;
+
+  // return new array
   return arr;
 }
+
+// test
 let arr = [1, 2, 3, 4, 5];
 let m = 2;
 let n = 1;
--- url_shorterner.js
+++ url_shorterner.js
@@ -1,30 +1,48 @@
-const urlDatabase = {}; // this object will store the mappings between short codes and long urls
+const urlStore = {
+  lfmi6alk2t0: 'https://youtube.com',
+  lfmi6alk35b: 'https://paypal.com',
 
-function generateShortCode(longUrl) {
+}; // this object will store the mappings between short codes and long urls
+
+// function to generate short code
+function generateShortCode() {
   const timestamp = new Date().getTime().toString(36); // use current timestamp as part of the code
   const random = Math.floor(Math.random() * 10000).toString(36); // use a random number as part of the code
   return `${timestamp}${random}`; // combine the timestamp and random number to create the short code
 }
 
+// main function
 function shortenUrl(longUrl) {
   // check if the long url already has a short code
-  for (const code in urlDatabase) {
-    if (urlDatabase[code] === longUrl) {
-      return `https://example.com/${code}`; // return the existing short url
+  for (const code in urlStore) {
+    if (urlStore[code] === longUrl) {
+      return `https://${code}`; // return the existing short url
     }
   }
 
   // generate a new short code
-  const shortCode = generateShortCode(longUrl);
-  urlDatabase[shortCode] = longUrl; // store the mapping in the database
-  return `https://example.com/${shortCode}`; // return the new short url
+  const shortCode = generateShortCode();
+  urlStore[shortCode] = longUrl; // store the mapping in the database
+  return `https://${shortCode}`; // return the new short url
 }
 
+// function to get expanded url
 function expandUrl(shortUrl) {
   const code = shortUrl.substring(shortUrl.lastIndexOf("/") + 1); // extract the short code from the url
-  if (urlDatabase[code]) {
-    return urlDatabase[code]; // return the original long url
+  if (urlStore[code]) {
+    return urlStore[code]; // return the original long url
   } else {
     throw new Error("Invalid short url"); // throw an error if the short url is invalid
   }
 }
+
+// test
+shortenUrl("https://google.com")
+shortenUrl("https://apple.com")
+shortenUrl("https://twitter.com")
+shortenUrl("https://paypal.com")
+shortenUrl("https://youtube.com")
+
+console.log(urlStore)
+
+console.log(expandUrl("lfmi6alk2t0"))
